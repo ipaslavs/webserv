ca6d7179ac8639ec9ec2243dbd221298b626b6d9
Isaac Hogben <ihogben@F3C004.42adel.org.au>
2024-06-27 16:28:53 +0930

    Added default config launch path.

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -2,12 +2,14 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <config_file>" << std::endl;
-        return EXIT_FAILURE;
-    }
+    std::string config_path = "config.yaml";
 
-    std::vector<ServerConfig> configs = parseConfigFile(argv[1]);
+    if (argc < 2) 
+        std::cerr << "Usage: " << argv[0] << config_path << std::endl;
+    else if (argv[1])
+        config_path = argv[1];
+
+    std::vector<ServerConfig> configs = parseConfigFile(config_path);
     if (configs.empty()) {
         std::cerr << "No valid server configurations found in the configuration file." << std::endl;
         return EXIT_FAILURE;
